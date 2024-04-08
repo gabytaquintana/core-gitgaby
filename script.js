92a6a6cdb6fb183280e2bc40cf17bba8a2ca8c5e
Gaby <gabriela@gmail.com>
2024-04-08 12:13:01 -0400

    core-likes

---
+++ script.js
@@ -0,0 +1,20 @@
+function aumentarLikes1() {
+    var content1= document.getElementById("MeGusta1");
+    var like1 = parseInt(content1.innerHTML);
+    content1.innerHTML = like1 + 1 + "like(s)"
+
+}
+
+function aumentarLikes2() {
+    var content2= document.getElementById("MeGusta2");
+    var like2 = parseInt(content2.innerHTML);
+    content2.innerHTML = like2 + 1 + "like(s)"
+
+}
+
+function aumentarLikes3() {
+    var content3= document.getElementById("MeGusta3");
+    var like3 = parseInt(content3.innerHTML);
+    content3.innerHTML = like3 + 1 + "like(s)"
+
+}
